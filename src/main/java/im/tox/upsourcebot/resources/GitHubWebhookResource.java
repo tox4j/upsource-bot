2b4272a8cc09fa30f2f8666c0b82614d51e37468
Simon Levermann <slever@fb3.uni-bremen.de>
2015-03-09 23:47:08 +0100

    Only greet on issue creation

--- src/main/java/im/tox/upsourcebot/resources/GitHubWebhookResource.java
+++ src/main/java/im/tox/upsourcebot/resources/GitHubWebhookResource.java
@@ -1,15 +1,12 @@
 package im.tox.upsourcebot.resources;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -30,10 +27,12 @@ public class GitHubWebhookResource {
 
   @POST
   public Response receiveHook(IssueWebhook payload) {
-    String senderName = payload.getSender().getLogin();
-    String message = "Hello, @" + senderName + ", nice to meet you! I am a robot.";
-    gitHubConnector.commentOnIssue(payload.getRepository().getFullName(), message,
-                                   payload.getIssue().getNumber());
+    if ("opened".equals(payload.getAction())) {
+      String senderName = payload.getSender().getLogin();
+      String message = "Hello, @" + senderName + ", nice to meet you! I am a robot.";
+      gitHubConnector.commentOnIssue(payload.getRepository().getFullName(), message,
+                                     payload.getIssue().getNumber());
+    }
     return Response.ok().build();
   }
 
